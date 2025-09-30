78d620086983648e5b167ab5c90a313454250d7a
Fede <fvalsagna@fi.uba.ar>
2025-09-30 18:52:35 -0300

    Eliminacion de seccion comentada

--- Ejercicio 1/ej1.py
+++ Ejercicio 1/ej1.py
@@ -26,30 +26,3 @@ def ej1_wrapper(arr):
 
 def medio(indice_inicial, indice_final):
     return (indice_final - indice_inicial) // 2 + indice_inicial
-
-# a = [-1,0,1,3,4]
-# b = [0,1,2,3,4,5]
-# c = [-4,2,3,4,5]
-# d = [-1,0,1,3]
-# e = [0,1,2,3,4]
-# f = [-4,2,3,4]
-# g = [0,2,3,4,5,6] # 0
-# h = [0,2,3,4,5] # 0
-# i = [-1,0,1,2,4] # 4
-# j = [-1,0,1,2,3,5] # 5
-# print(ej1_wrapper(a))
-# print(ej1_wrapper(b))
-# print(ej1_wrapper(c))
-# print(ej1_wrapper(d))
-# print(ej1_wrapper(e))
-# print(ej1_wrapper(f))
-# print(ej1_wrapper(g))
-# print(ej1_wrapper(h))
-# print(ej1_wrapper(i))
-# print(ej1_wrapper(j))
-
-# medio(0,7) # 0 1 2 3 4 5 6 7
-# medio(0,6) # 0 1 2 3 4 5 6
-# medio(2,8) # 0 1 2 3 4 5 6 7 8 
-# medio(2,7) 
-# print(medio(3,5))
