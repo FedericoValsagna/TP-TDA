88df1a04d2eed24d874d3170fd6c429745bdc5cd
Federico Valsagna <fvalsagna@fi.uba.ar>
2025-10-03 14:04:34 -0300

    Bugfixes

--- Ejercicio 4/ej4.py
+++ Ejercicio 4/ej4.py
@@ -19,8 +19,12 @@ def ej4(arreglo):
     suma_resultado = [0] * len(arreglo) 
     suma_actual = [0] * len(arreglo)
 
-    suma_resultado[0] = arreglo[0]
-    suma_actual[0] = arreglo[0]
+    if arreglo[0] > 0:
+        suma_resultado[0] = arreglo[0]
+        suma_actual[0] = arreglo[0]
+    else:
+        suma_resultado[0] = 0
+        suma_actual[0] = 0
 
     for i in range(1, len(arreglo)):
         suma_en_paso_actual = suma_actual[i-1] + arreglo[i]
@@ -30,22 +34,37 @@ def ej4(arreglo):
             suma_actual[i] = suma_en_paso_actual
  
         suma_resultado[i] = max(suma_actual[i], suma_resultado[i -1])
+    print(f" A: {arreglo}")
+    print(f"SR: {suma_resultado}")
+    print(f"SA: {suma_actual}")
     return reconstruccion(suma_resultado, suma_actual, arreglo), suma_resultado[-1]
 
         
 def reconstruccion(suma_resultado, suma_actual, arreglo):
     inicio = True
     subarreglo = []
-    for i in range(len(arreglo) - 1,1 ,-1):
+    for i in range(len(arreglo) - 1, -1,-1):
+        # La idea es que el primer elemento del subarreglo se identifica desde la sumatoria total, los subsiguientes se identifican a partir
+        # no del total sino del acumulado.
         if inicio:
             if suma_resultado[i] - arreglo[i] != suma_actual[i - 1]:
                 continue
             subarreglo.append(arreglo[i])
+
+            # Este corte es por si el subarreglo es de largo 1
+            if suma_actual[i- 1] == 0:
+                break
             inicio = False
         else:
+            # Si la sumatoria actual menos el item actual dan 0 eso indica que estamos en el comienzo de la sumatoria, por lo cual
+            # encontramos el subarreglo máximo
             if suma_actual[i] - arreglo[i] == 0:
                 subarreglo.append(arreglo[i])
-                subarreglo.reverse()
-                return subarreglo
+                break
             else:
+                # Acá tomamos en cuenta que estamos en un punto medio del subarreglo máximo, en ese caso todo elemento intermedio entre el comienzo
+                # y fin del subarreglo automáticamente se encuentra dentro del subarreglo ya que debe ser continuo.
                 subarreglo.append(arreglo[i])
+
+    subarreglo.reverse()
+    return subarreglo
