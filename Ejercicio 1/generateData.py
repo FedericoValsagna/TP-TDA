bde3e194d7507176ee0b0364aa597db9be115e83
Nicolas Riedel <nriedel@fi.uba.ar>
2025-10-04 01:57:08 -0300

    improvement in performance to ejercicio 1

--- Ejercicio 1/generateData.py
+++ Ejercicio 1/generateData.py
@@ -1,62 +1,35 @@
 import random
-import os
 from main import ej1_wrapper
 
-random.seed(2025)
+random.seed(2000)
+
+CASOS_POR_TAMANO = 5
 
 casos_base = [
-    "-1,0,1,3,4 / 3",
-    "0,1,2,3,4,5 / 2",
-    "-4,2,3,4,5 / -1",
-    "-1,0,1,3 / 3",
-    "0,1,3,4 / 1",
-    "-4,2,3,4 / -1",
-    "0,2,3,4,5,6 / 0",
-    "0,2,3,4,5 / 0",
-    "-1,0,1,2,4 / 4",
-    "-1,0,1,2,3,5 / 5"
+    ([-1,0,1,3,4], 3),
+    ([0,1,2,3,4,5], 2),
+    ([-4,2,3,4,5], -1),
+    ([-1,0,1,3], 3),
+    ([0,1,3,4], 1),
+    ([-4,2,3,4], -1),
+    ([0,2,3,4,5,6], 0),
+    ([0,2,3,4,5], 0),
+    ([-1,0,1,2,4], 4),
+    ([-1,0,1,2,3,5], 5)
 ]
 
+tamanos = [25_000_000, 50_000_000, 75_000_000, 100_000_000, 125_000_000, 150_000_000, 175_000_000, 200_000_000]
+
 def generar_arreglo_ordenado(tamano):
-    arreglo = []
     valor_inicial = random.randint(-tamano, tamano)
-    forzar_punto_fijo = random.choice([True, False, False, False])  # 25% de probabilidad
-    if forzar_punto_fijo :
-        valor_inicial = 0
+    return list(range(valor_inicial, valor_inicial + tamano))
 
-    for i in range(tamano):
-        arreglo.append(valor_inicial + i)
-
-    return arreglo
-
-def generar_arreglos_de_prueba():
-    casos_de_prueba = casos_base.copy()
-    tamanos = [5_000_000, 10_000_000, 15_000_000, 20_000_000, 25_000_000, 30_000_000, 35_000_000, 40_000_000, 45_000_000, 50_000_000]  # Tamaños grandes para pruebas de rendimiento
+def generar_casos_de_prueba():
+    for caso in casos_base:
+        yield caso
 
     for tamano in tamanos:
-        arreglo = generar_arreglo_ordenado(tamano)
-        resultado_esperado = ej1_wrapper(arreglo)
-        caso_de_prueba = f"{', '.join(map(str, arreglo))} / {resultado_esperado}"
-        casos_de_prueba.append(caso_de_prueba)
-
-    return casos_de_prueba
-
-def escribir_archivo_de_prueba(test_cases, filename):
-    filepath = os.path.join(os.path.dirname(__file__), filename)
-    
-    with open(filepath, 'w') as f:
-        for test_case in test_cases:
-            f.write(test_case + '\n')
-    
-    print(f"Total casos de prueba generados: {len(test_cases)}")
-
-
-def generar_casos_de_prueba(filename="tests.txt"):
-    if os.path.exists(filename):
-        print(f"El archivo {filename} ya existe, en caso de querer generar nuevos casos, elimine el archivo")
-        return
-    test_cases = generar_arreglos_de_prueba()
-    escribir_archivo_de_prueba(test_cases, filename)
-
-if __name__ == "__main__":
-    generar_casos_de_prueba()
+        for _ in range(CASOS_POR_TAMANO):
+            arreglo = generar_arreglo_ordenado(tamano)
+            resultado_esperado = ej1_wrapper(arreglo)
+            yield (arreglo, resultado_esperado)
