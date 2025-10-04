74b4aa6aeda9f7d286ee2ef78e5c30a31d635aff
Nicolas Riedel <nriedel@fi.uba.ar>
2025-10-03 23:54:13 -0300

    change generateData of ej1

--- Ejercicio 1/generateData.py
+++ Ejercicio 1/generateData.py
@@ -1,11 +1,12 @@
 import random
 import os
+from main import ej1_wrapper
 
 random.seed(2025)
 
 casos_base = [
     "-1,0,1,3,4 / 3",
-    "0,1,2,3,4,5 / 2", 
+    "0,1,2,3,4,5 / 2",
     "-4,2,3,4,5 / -1",
     "-1,0,1,3 / 3",
     "0,1,3,4 / 1",
@@ -16,38 +17,28 @@ casos_base = [
     "-1,0,1,2,3,5 / 5"
 ]
 
-def generar_arreglo_ordenado_con_punto_fijo(tamano):
+def generar_arreglo_ordenado(tamano):
     arreglo = []
+    valor_inicial = random.randint(-tamano, tamano)
+    forzar_punto_fijo = random.choice([True, False, False, False])  # 25% de probabilidad
+    if forzar_punto_fijo :
+        valor_inicial = 0
 
-    tiene_punto_fijo = random.choice([True, False])
-    
-    if tiene_punto_fijo:
-        indice_punto_fijo = random.randint(0, tamano - 1)
-        
-        for i in range(tamano):
-            if i < indice_punto_fijo:
-                arreglo.append(i - random.randint(1, 5))
-            elif i == indice_punto_fijo:
-                arreglo.append(i)
-            else:
-                arreglo.append(i + random.randint(1, 5))
-        
-        return arreglo, indice_punto_fijo
-    else:
-        offset = random.randint(1, 5)
-        for i in range(tamano):
-            arreglo.append(i + offset)
-        return arreglo, -1
+    for i in range(tamano):
+        arreglo.append(valor_inicial + i)
+
+    return arreglo
 
 def generar_arreglos_de_prueba():
     casos_de_prueba = casos_base.copy()
     tamanos = [5, 10, 1000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000]
-    
+
     for tamano in tamanos:
-        arreglo, resultado_esperado = generar_arreglo_ordenado_con_punto_fijo(tamano)
+        arreglo = generar_arreglo_ordenado(tamano)
+        resultado_esperado = ej1_wrapper(arreglo)
         caso_de_prueba = f"{', '.join(map(str, arreglo))} / {resultado_esperado}"
         casos_de_prueba.append(caso_de_prueba)
-    
+
     return casos_de_prueba
 
 def escribir_archivo_de_prueba(test_cases, filename):
